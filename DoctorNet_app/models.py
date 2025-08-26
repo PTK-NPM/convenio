56d970cbd6b118a58c8b65bd050beb9a637b572d
PTK-NPM <patricknunes520@gmail.com>
2025-08-26 10:03:48 -0300

    Update models.py

--- DoctorNet_app/models.py
+++ DoctorNet_app/models.py
@@ -19,6 +19,7 @@ class Beneficiario(models.Model):
 class Procedimento(models.Model):
     codigo_procedimento = models.CharField("Código do procedimento", max_length = 8, unique = True)
     nome = models.CharField("Nome", max_length = 250)
+    quantidade_procedimento = models.Charfield("Quantidade", max_length = 2, required = True)
     class Categoria(models.TextChoices):
         IMAGEM = 'IMG', 'Exame de Imagem'
         LABORATORIAL = 'LAB', 'Exame Laboratorial'
